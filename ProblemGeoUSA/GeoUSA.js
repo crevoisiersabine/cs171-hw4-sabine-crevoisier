8b55b8f0a5d93dda8964606da0a56808d3d00d62
Sabine <crevoisiersabine@yahoo.fr>
2014-09-15 21:35:06 +0700

    loaded station data with aggregated values per month, added tooltip hover

--- ProblemGeoUSA/GeoUSA.js
+++ ProblemGeoUSA/GeoUSA.js
@@ -39,49 +39,71 @@ var path = d3.geo.path().projection(projection);
 var centered;
 
 var dataSet = {};
+var january_data = {}
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>Station:</strong> <span style='color:steelblue'>" + d.state + "</span>";
+  });
+
+svg.call(tip);
 
 function loadStations() {
     d3.csv("../data/NSRDB_StationsMeta.csv",function(error,data){
-        var stations = [];
-        data.forEach(function(d){
-            station = {USAF: "", latitude: 0, longitude: 0, state: ""};
-            station.USAF = d['USAF'];
-            station.latitude = parseFloat(String(d['NSRDB_LAT (dd)']));
-            station.longitude = parseFloat(String(d['NSRDB_LON(dd)']));
-            station.state = d['STATION'];
-            if (projection([station.longitude, station.latitude]) != null){
-                stations.push(station);
+        d3.json("../data/reducedMonthStationHour2003_2004.json", function(error,json){
+            completeDataSet = json['Nov'];
+            for (station_name in completeDataSet){
+                january_data[station_name] = completeDataSet[station_name].sum
             }
-        })
-        svg.selectAll(".station")
-            .data(stations)
-            .enter().append("circle")
-            .attr("class", "station")
-            .attr("r", 2)
-            .attr("transform", function(d) {
-                return "translate(" + projection([d.longitude, d.latitude]) + ")"
+
+            var stations = [];
+            data.forEach(function(d){
+                station = {USAF: "", latitude: 0, longitude: 0, state: ""};
+                station.USAF = d['USAF'];
+                station.latitude = parseFloat(String(d['NSRDB_LAT (dd)']));
+                station.longitude = parseFloat(String(d['NSRDB_LON(dd)']));
+                station.state = d['STATION'];
+                if (projection([station.longitude, station.latitude]) != null){
+                    stations.push(station);
+                }
             })
+            stations.forEach(function(d){
+                for (has_data in january_data){
+                    if (d.USAF == has_data && parseFloat(String(january_data[has_data])) != 0.0){
+                        d['radius'] = Math.pow(1.2, Math.pow(parseFloat(String(january_data[has_data])), 1/6));
+                    }
+                    else{
+                        if(d.radius == undefined){ //only assign a radius if the datapoint hasn't already got one to avoid overwriting
+                            d['radius'] = 2;
+                        }
+                    }
+                }
+            })
+
+
+            svg.selectAll(".station")
+                .data(stations)
+                .enter().append("circle")
+                .attr("r", function(d){ return d.radius; })
+                .attr("class", function(d){
+                    if (d.radius == 2) return "station";
+                    else return "station hasData"
+                })
+                .attr("transform", function(d) {
+                    return "translate(" + projection([d.longitude, d.latitude]) + ")"
+                })
+                .on('mouseover', tip.show)
+                .on('mouseout', tip.hide);
+        });
     });
-}
-
-
-function loadStats() {
-
-    d3.json("../data/reducedMonthStationHour2003_2004.json", function(error,data){
-        completeDataSet= data;
-
-		//....
-		
-        loadStations();
-    })
-
 }
 
 
 d3.json("../data/us-named.json", function(error, data) {
 
     var usMap = topojson.feature(data,data.objects.states).features
-    // console.log(usMap);
 
     svg.append("g").attr("id", "states")
         .selectAll(".country")
@@ -92,7 +114,6 @@ d3.json("../data/us-named.json", function(error, data) {
     // see also: http://bl.ocks.org/mbostock/4122298
 
     loadStations();
-    // var screencoord = projection([longitude, latitude]);
 });
 
 // ZOOMING
